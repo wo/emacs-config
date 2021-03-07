37d981acd44ef3dba73ca45850ef109abc92f1cb
wo <wo@umsu.de>
2021-03-07 10:08:41 +0000

    use latexmk

--- init.el
+++ init.el
@@ -135,7 +135,7 @@
        json
        ;;julia             ; a better, faster MATLAB
        ;;kotlin            ; a better, slicker Java(Script)
-       latex             ; writing papers in Emacs has never been so fun
+       (latex +latexmk)             ; writing papers in Emacs has never been so fun
        ;;lean
        ;;factor
        ;;ledger            ; an accounting system in Emacs
